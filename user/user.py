f7ea9198d8336ce7d48fe83419633fb2a8e4fc11
Sebastian Romero <sebastian.romero@imt-atlantique.net>
2022-10-01 10:34:21 +0200

    TP vert -> 5 et 6

--- user/user.py
+++ user/user.py
@@ -11,10 +11,41 @@ HOST = '0.0.0.0'
 with open('{}/databases/users.json'.format("."), "r") as jsf:
    users = json.load(jsf)["users"]
 
+def existsUser(userId):
+   for user in users:
+        if str(user["id"]) == str(userId): return True
+   return False
+
 @app.route("/", methods=['GET'])
 def home():
    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
 
+@app.route("/bookings/<nameorid>", methods=['GET'])
+def get_bookings_bynameorid(nameorid):
+   for user in users:
+        if str(user["name"]) == str(nameorid) or str(user["id"]) == str(nameorid):
+            return make_response(jsonify(requests.get('http://booking:3201/bookings/'+user["id"]).json()),200)
+   return make_response(jsonify({"error":"user not found"}),400)
+
+@app.route("/bookings/<userid>", methods=['POST'])
+def create_booking(userid):
+   req = request.get_json()
+   if existsUser(userid):
+      return make_response(jsonify(requests.post('http://booking:3201/bookings/'+userid, json=req).json()),200)
+   return make_response(jsonify({"error":"user not found"}),400)
+
+@app.route("/movies/<userid>", methods=['GET'])
+def get_info_movies(userid):
+   if existsUser(userid):
+      bookings = requests.get('http://booking:3201/bookings/'+userid).json()
+      for date in bookings["dates"]:
+         movies= []
+         for movieid in date["movies"]:
+            movies.append(requests.get('http://movie:3200/movies/'+movieid).json())
+         date["movies"] = movies
+
+      return make_response(jsonify(bookings),200)
+   return make_response(jsonify({"error":"user not found"}),400)
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
